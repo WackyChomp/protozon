5bcd3869e20e84f4d18e0fdac255a1e7e57d4797
Wilson Chan <wilsonchan54@gmail.com>
2025-01-07 21:41:47 -0500

    Destructed props from posts object in root home page.tsx / added styling and rendered posts values idea card component / updated next.config.ts

--- components/IdeaCard.tsx
+++ components/IdeaCard.tsx
@@ -1,30 +1,63 @@
 
 import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
 import { formatDate } from "@/lib/utils";
 
 import { EyeIcon } from "lucide-react"
 
+// custom class: flex-between
+
 const IdeaCard = ({ post }: { post: IdeaTypeCard}) => {
+
+  // Destructured props from root home page.tsx
+  const { _createdAt, views, author: { _id: authorId, name }, title , description, category, _id, image } = post;
+
   return (
-    <li className="idea-card group">
+    <li className="idea_card group">
       <div className="flex-between">
         <p className="idea_card_date">
-          {formatDate(post._createdAt)}
+          {formatDate(_createdAt)}
         </p>
 
         <div className="flex gap-2">
           <EyeIcon className='size-7 text-lime-500'/>
-          <span className="text-16-medium">{post.views}</span>
-        </div>
-
-        <div className="flex-between mt-5 gap-5">
-          <div className="flex-1">
-            <Link href={`/user/${post.author?._id}`}>
-              <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
-            </Link>
-          </div>
+          <span className="text-16-medium">{views}</span>
         </div>
       </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+        <Link href={`/user/${authorId}`}>
+          <Image src='https://placehold.co/48x48' alt='image placeholder' width={50} height={50} className="rounded-full border-2 border-blue-500" />
+        </Link>
+      </div>
+
+      <Link href={`/user/${authorId}`}>
+        <p className="idea_card_desc">{description}</p>
+        <img src={image} alt='placeholder' className="idea_card_img" />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className='idea_card_btn'>
+          <Link href={`/startup/${_id}`}>
+            Details
+          </Link>
+        </Button>
+      </div>
+
     </li>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    dangerouslyAllowSVG: true,
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: '*',          // allows from all sources
+      }
+    ]
+  }
 };
 
 export default nextConfig;
