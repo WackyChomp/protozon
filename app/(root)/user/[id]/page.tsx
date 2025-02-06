913d1f9476ff022fa09fd86cceea11545661c7c4
Wilson Chan <wilsonchan54@gmail.com>
2025-02-05 23:10:47 -0500

    Added profile styling in globals.css / rendered profile details in individual user id page.tsx

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { auth } from "@/auth";
+import { notFound } from "next/navigation";
+import Image from "next/image";
 import { client } from '@/sanity/lib/client';
 import { AUTHOR_BY_ID_QUERY } from "@/lib/queries";
-import { notFound } from "next/navigation";
 
 
 const Page = async ({ params }: { params: Promise<{id:string}> }) => {
@@ -13,12 +14,41 @@ const Page = async ({ params }: { params: Promise<{id:string}> }) => {
   if(!user) return notFound()
 
   return (
-    <section>
+    <section className="profile_container lg:flex-row">
      <div className="bg-red-600 flex flex-between p-3">
        <div>User Page</div>
        <div>Name: {user.name}</div>
        <div>User Id: {id}</div>
       </div>
+
+      <div className="profile_card max-lg:w-full">
+        <div className="profile_title">
+          <h3 className="text-2xl font-extrabold uppercase text-center line-clamp-1">
+            {user.name}
+          </h3>
+        </div>
+
+        <Image 
+          src={user.image}
+          alt={user.name}
+          width={200}
+          height={200}
+        />
+
+        <p className="text-1xl font-extrabold mt-8 text-center">
+          @{user?.username}
+        </p>
+        <p className="mt-1 text-center text-14-normal">
+          {user?.bio}
+        </p>
+      </div>
+
+      <div className="flex flex-1 flex-col gap-5 lg:mt-5">
+        <p className="text-2xl font-bold">
+          {session ?.id == id ? 'Your' : 'All'} Ideas
+        </p>
+      </div>
+      
     </section>
   )
 }
